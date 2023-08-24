2971909f6340c687694b02461154843d987c8f21
何全 <hequan2011@sina.com>
2023-08-24 20:22:46 +0800

    1.1

--- applications/luci-app-forcedata/luasrc/model/cbi/forcedata.lua
+++ applications/luci-app-forcedata/luasrc/model/cbi/forcedata.lua
@@ -6,7 +6,7 @@ local taskd = require "luci.model.tasks"
 local forcedata_model = require "luci.model.forcedata"
 local m, s, o
 
-m = taskd.docker_map("forcedata", "forcedata", "chmod +x /usr/libexec/istorec/forcedata.sh  &&  /usr/libexec/istorec/forcedata.sh ",
+m = taskd.docker_map("forcedata", "forcedata", "who  &&  pwd   &&  /usr/libexec/istorec/forcedata.sh ",
 	translate("Forcedata"),
 	"通过简单安装后可快速加入原力联盟的边缘计算生态，在线共享带宽即可赚钱，每月可获取一定的现金回报！了解更多，请登录「<a href=\"http://www.forcedata.cn\" target=\"_blank\" >原力联盟官网</a>」并查看<a href=\"https://docs.qq.com/doc/DZXNFWWtKbHN4UHFD\" target=\"_blank\">「教程」</a>")
 
