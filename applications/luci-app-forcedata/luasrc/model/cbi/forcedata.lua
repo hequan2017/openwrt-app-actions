58260eee13ee15601b94dac3cfe94fafc07ac76d
何全 <hequan2011@sina.com>
2023-08-29 16:03:11 +0800

    8.28.2

--- applications/luci-app-forcedata/luasrc/model/cbi/forcedata.lua
+++ applications/luci-app-forcedata/luasrc/model/cbi/forcedata.lua
@@ -8,7 +8,7 @@ local m, s, o
 
 m = taskd.docker_map("forcedata", "forcedata","/usr/libexec/istorec/forcedata.sh",
 	translate("Forcedata"),
-	"通过简单安装后可快速加入原力联盟的边缘计算生态，在线共享带宽即可赚钱，每月可获取一定的现金回报！了解更多，请登录「<a href=\"http://www.forcedata.cn\" target=\"_blank\" >原力联盟官网</a>」并查看<a href=\"https://docs.qq.com/doc/DZXNFWWtKbHN4UHFD\" target=\"_blank\">「教程」</a>")
+	"通过简单安装后可快速加入原力云的边缘计算生态，在线共享带宽即可赚钱，每月可获取一定的现金回报！了解更多，请登录「<a href=\"http://www.forcedata.cn\" target=\"_blank\" >原力云官网</a>」并查看<a href=\"https://docs.qq.com/doc/DZXNFWWtKbHN4UHFD\" target=\"_blank\">「教程」</a>")
 
 s = m:section(SimpleSection, translate("Service Status"), translate("Forcedata status:"), "")
 s:append(Template("forcedata/status"))
